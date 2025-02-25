22f48851ed55ff8a4f7a5b47245e2b41ab662cab
xle6yc3k <huntergeorgod@gmail.com>
2025-02-26 00:18:08 +0300

    Исправлен роутинг для GitHub Pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import HomePage from '../pages/HomePage.vue';
 import ResumeBuilder from '../pages/ResumeBuilderPage.vue';
 import AnalysePage from '../pages/AnalysePage.vue';
